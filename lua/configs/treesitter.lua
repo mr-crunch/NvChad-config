869beb4ea80a5fe9540fcc41b8b26d5819cb243f
Matt Bianco <m2air@Matts-MacBook-Air.local>
2024-10-08 19:26:32 -0500

    initial commit

---
+++ lua/configs/treesitter.lua
@@ -0,0 +1,26 @@
+local options = {
+	ensure_installed = {
+		"bash",
+		"c",
+		"cmake",
+		"cpp",
+		"lua",
+		"luadoc",
+		"markdown",
+		"make",
+		"printf",
+		"toml",
+		"vim",
+		"vimdoc",
+		"yaml",
+	},
+
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
+
+	indent = { enable = true },
+}
+
+require("nvim-treesitter.configs").setup(options)
